39fb04a7f2c65a7ec1a0d5fd2f22292ad095bd1b
Vedant Khairnar <vedron007@gmail.com>
2020-07-15 00:46:21 +0530

    Bug Fixed!!

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ def get_query_string():
         data = json.load(f) 
         f.close() 
         query = request.args.getlist('key')[0]
-        
+
         for i in data["intents"]:
             if query in i['patterns']:
                 return random.choice(i['responses'])
-            else:
-                raise("Bad Input Error")    
+        else:
+            raise("Bad Input Error")    
     except :
         return "Bad Input Error"
 
