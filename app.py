199890e3b5182932abb7aa7d217ecd5c547e993c
Vedant Khairnar <vedron007@gmail.com>
2020-07-15 00:17:19 +0530

    Code 1.0 Added

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,request,jsonify
+import random
+import json 
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_query_string():
+    try:
+        f = open('intents.json',) 
+        data = json.load(f) 
+        f.close() 
+        query = request.args.getlist('key')[0]
+        query = "Hi"
+        for i in data["intents"]:
+            if query in i['patterns']:
+                return random.choice(i['responses'])
+            else:
+                raise("Bad Input Error")    
+    except :
+        return "Bad Input Error"
+
+if __name__ == '__main__':
+    app.run(debug=True)
